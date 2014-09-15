e463219a9492a3e79224e148a89752292710eb16
jose miguel mut <jm_mut@hotmail.com>
2014-09-15 15:02:06 +0100

    Cryptojs: successfully stored and retrieved.

--- src/lib/data-adapter/feature/cellbase-adapter.js
+++ src/lib/data-adapter/feature/cellbase-adapter.js
@@ -186,14 +186,14 @@ CellBaseAdapter.prototype = {
             regions.push(new Region(queryResult.id));
             chunks.push(queryResult.result);
         }
-        chunks = this.cache[combinedCacheId].putByRegions(regions, this._encryptChunks(chunks, "mypassword"), true);
 
+        var items = this.cache[combinedCacheId].putByRegions(regions, this._encryptChunks(chunks, "mypassword"), true);
+        var decItems = this._decryptChunks(items, "mypassword");
         /** time log **/
         console.timeEnd(timeId);
 
-
         if (chunks.length > 0) {
-            this.trigger('data:ready', {items: chunks, dataType: dataType, chunkSize: chunkSize, sender: this});
+            this.trigger('data:ready', {items: decItems, dataType: dataType, chunkSize: chunkSize, sender: this});
         }
         if (args.webServiceCallCount === 0) {
             args.done();
@@ -221,8 +221,9 @@ CellBaseAdapter.prototype = {
             }
         }
         var items = this.cache[combinedCacheId].putByRegions(regions, this._encryptChunks(chunks, "mypassword"), true, histogramId); // TODO remove "histogram" from "_histogram_<interval>"
+        var decItems = this._decryptChunks(items, "mypassword");
 
-        this.trigger('data:ready', {items: items, dataType: dataType, chunkSize: chunkSize, sender: this});
+        this.trigger('data:ready', {items: decItems, dataType: dataType, chunkSize: chunkSize, sender: this});
         if (args.webServiceCallCount === 0) {
             args.done();
         }
@@ -232,7 +233,8 @@ CellBaseAdapter.prototype = {
         var decryptedChunks = [];
         for (var i = 0; i < chunks.length; i++) {
             if (chunks[i].enc == true) {
-                decryptedChunks.push(CryptoJS.AES.decrypt(chunks[i], password));
+                var decrypt = CryptoJS.AES.decrypt(chunks[i].value, password, {format: JsonFormatter});
+                decryptedChunks.push({chunkKey: chunks[i].chunkKey, value: JSON.parse(decrypt.toString(CryptoJS.enc.Utf8))});
             } else {
                 decryptedChunks.push(chunks);
             }
@@ -242,10 +244,55 @@ CellBaseAdapter.prototype = {
 
     _encryptChunks: function (chunks, password) {
         var encryptedChunks = [];
+
         for (var i = 0; i < chunks.length; i++) {
-            encryptedChunks.push(CryptoJS.AES.encrypt(chunks[i], password));
+            var message = JSON.stringify(chunks[i]);
+            var encr = CryptoJS.AES.encrypt(message, password, {format: JsonFormatter});
+            encryptedChunks.push(encr.toString());
+//            encryptedChunks.push(encr);
+//            encryptedChunks.push(chunks[i]);
         }
         return encryptedChunks;
     }
+
 };
 
+var JsonFormatter = {
+    stringify: function (cipherParams) {
+        // create json object with ciphertext
+        var jsonObj = {
+            ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64)
+        };
+
+        // optionally add iv and salt
+        if (cipherParams.iv) {
+            jsonObj.iv = cipherParams.iv.toString();
+        }
+        if (cipherParams.salt) {
+            jsonObj.s = cipherParams.salt.toString();
+        }
+
+        // stringify json object
+        return JSON.stringify(jsonObj);
+    },
+
+    parse: function (jsonStr) {
+        // parse json string
+        var jsonObj = JSON.parse(jsonStr);
+
+        // extract ciphertext from json object, and create cipher params object
+        var cipherParams = CryptoJS.lib.CipherParams.create({
+            ciphertext: CryptoJS.enc.Base64.parse(jsonObj.ct)
+        });
+
+        // optionally extract iv and salt
+        if (jsonObj.iv) {
+            cipherParams.iv = CryptoJS.enc.Hex.parse(jsonObj.iv)
+        }
+        if (jsonObj.s) {
+            cipherParams.salt = CryptoJS.enc.Hex.parse(jsonObj.s)
+        }
+
+        return cipherParams;
+    }
+};
