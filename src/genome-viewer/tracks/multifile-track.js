d3dbf1490e9c5f122f3060195496dd181ae81035
jose miguel mut <jm_mut@hotmail.com>
2014-11-10 14:39:03 +0000

    Renamed MutlisampleTrack to multifileTrack.

--- src/genome-viewer/tracks/multifile-track.js
+++ src/genome-viewer/tracks/multifile-track.js
@@ -19,9 +19,9 @@
  * along with JS Common Libs. If not, see <http://www.gnu.org/licenses/>.
  */
 
-MultisampleTrack.prototype = new Track({});
+MultifileTrack.prototype = new Track({});
 
-function MultisampleTrack(args) {
+function MultifileTrack(args) {
     Track.call(this, args);
 
     // Using Underscore 'extend' function to extend and add Backbone Events
@@ -36,22 +36,22 @@ function MultisampleTrack(args) {
 //    this.histogramRenderer = new FeatureClusterRenderer();
     var histogramArgs = _.extend({}, args);
     this.histogramRenderer = new HistogramRenderer(_.extend(histogramArgs, {histogramMaxFreqValue: 200,
-        height: args.height/args.samples.length}));
+        height: args.height/args.files.length}));
 
     this.featureType = 'Feature';
-    this.samples = [];
+    this.files = [];
     //set instantiation args, must be last
     _.extend(this, args);
 
     this.renderers = {};
     for (var i = 0; i < this.featureTypes.length; i++) {
         if (this.featureTypes[i] == this.ALIGNMENT_FEATURE && this.alignmentRenderer) {
-            this.renderers[this.samples[i]] = this.alignmentRenderer;
+            this.renderers[this.files[i]] = this.alignmentRenderer;
         } else if (this.featureTypes[i] == this.VARIANT_FEATURE && this.variantRenderer) {
-            this.renderers[this.samples[i]] = this.variantRenderer;
+            this.renderers[this.files[i]] = this.variantRenderer;
         } else {
-            this.renderers[this.samples[i]] = null;
-            console.log("no renderer provided for sample " + this.samples[i]);
+            this.renderers[this.files[i]] = null;
+            console.log("no renderer provided for file " + this.files[i]);
         }
     }
 
@@ -60,64 +60,64 @@ function MultisampleTrack(args) {
 
     this.dataType = 'features';
 
-    this.sampleDivs = {};   // <this.samples.length> children of contentdiv.
-    this.sampleMainSvgs = {};   // bijection 1 to 1 with sampleDivs
+    this.fileDivs = {};   // <this.files.length> children of contentdiv.
+    this.fileMainSvgs = {};   // bijection 1 to 1 with fileDivs
     this.svgGroups = {};    // wrapper of renderer-related internal svgs.
-    // TODO this.renderer.setSamples(this.samples);
+    // TODO this.renderer.setfiles(this.files);
 
     this.chunksDisplayed = new FeatureChunkCache({storeType: "MemoryStore"});
 };
 
-MultisampleTrack.prototype.updateHeight = function () {
+MultifileTrack.prototype.updateHeight = function () {
     //TODO if needed
 
     $(this.contentDiv).css({'height': this.height});
-    var sampleHeight = this.height/this.samples.length;
+    var fileHeight = this.height/this.files.length;
 
     if (this.histogram) {
-        for (var j = 0; j < this.samples.length; j++) {
-            var sample = this.samples[j];
+        for (var j = 0; j < this.files.length; j++) {
+            var file = this.files[j];
 //        $(this.contentDiv).css({'height': this.histogramRenderer.histogramHeight + 5});
-            $(this.sampleDivs[sample]).css({'height': sampleHeight});
-            this.sampleMainSvgs[sample].setAttribute('height', this.histogramRenderer.histogramHeight);
-            this.svgGroups[sample].setAttribute('height', this.histogramRenderer.histogramHeight);
+            $(this.fileDivs[file]).css({'height': fileHeight});
+            this.fileMainSvgs[file].setAttribute('height', this.histogramRenderer.histogramHeight);
+            this.svgGroups[file].setAttribute('height', this.histogramRenderer.histogramHeight);
         }
         return;
     }
 
-    for (var j = 0; j < this.samples.length; j++) {
-        var sample = this.samples[j];
-        $(this.sampleDivs[sample]).css({'height': sampleHeight});
-        // TODO this.sampleMainSvgs[sample].height = sampleHeight;
-        var renderedHeight = this.svgGroups[sample].getBoundingClientRect().height;
-        this.sampleMainSvgs[sample].setAttribute('height', renderedHeight);
+    for (var j = 0; j < this.files.length; j++) {
+        var file = this.files[j];
+        $(this.fileDivs[file]).css({'height': fileHeight});
+        // TODO this.fileMainSvgs[file].height = fileHeight;
+        var renderedHeight = this.svgGroups[file].getBoundingClientRect().height;
+        this.fileMainSvgs[file].setAttribute('height', renderedHeight);
     }
 
 
     if (this.resizable) {
         if (this.autoHeight == true) {
             var heightSum = 0;
-            for (var k = 0; k < this.samples.length; k++) {
-                var sample = this.samples[k];
+            for (var k = 0; k < this.files.length; k++) {
+                var file = this.files[k];
                 /*
                 var x = this.pixelPosition;
                 var width = this.width;
                 var lastContains = 0;
-                for (var i in this.renderedArea[sample]) {
-                    if (this.renderedArea[sample][i].contains({start: x, end: x + width })) {
+                for (var i in this.renderedArea[file]) {
+                    if (this.renderedArea[file][i].contains({start: x, end: x + width })) {
                         lastContains = k;
                     }
                 }
                 var visibleHeight = parseInt(lastContains) + 30;
-//                this.sampleMainSvgs[sample].setAttribute('height', visibleHeight);
+//                this.fileMainSvgs[file].setAttribute('height', visibleHeight);
                 heightSum += visibleHeight;
                 */
 
-                var renderedHeight = this.svgGroups[sample].getBoundingClientRect().height;
+                var renderedHeight = this.svgGroups[file].getBoundingClientRect().height;
                 if (renderedHeight > 200) {
                     renderedHeight = 200;
                 }
-                $(this.sampleDivs[sample]).css({'height': renderedHeight});
+                $(this.fileDivs[file]).css({'height': renderedHeight});
                 heightSum += renderedHeight;
 
             }
@@ -129,57 +129,57 @@ MultisampleTrack.prototype.updateHeight = function () {
 
 };
 
-MultisampleTrack.prototype.clean = function () {
+MultifileTrack.prototype.clean = function () {
 //    this._clean();
 
     this.chunksDisplayed = new FeatureChunkCache({storeType: "MemoryStore"});
     this.renderedArea = {};
 
 //    console.time("-----------------------------------------empty");
-    for (var i = 0; i < this.samples.length; i++) {
-        var svgCanvasFeatures = this.svgGroups[this.samples[i]];
+    for (var i = 0; i < this.files.length; i++) {
+        var svgCanvasFeatures = this.svgGroups[this.files[i]];
         while (svgCanvasFeatures.firstChild) {
             svgCanvasFeatures.removeChild(svgCanvasFeatures.firstChild);
         }
-        this.renderedArea[this.samples[i]] = {};
+        this.renderedArea[this.files[i]] = {};
 
-        if (this.renderers[this.samples[i]]) {
-            this.renderers[this.samples[i]].init(svgCanvasFeatures, this.samples[i]);
+        if (this.renderers[this.files[i]]) {
+            this.renderers[this.files[i]].init(svgCanvasFeatures, this.files[i]);
         }
     }
 //    console.timeEnd("-----------------------------------------empty");
 };
 
-MultisampleTrack.prototype.render = function (targetId) {
+MultifileTrack.prototype.render = function (targetId) {
     var _this = this;
     this.initializeDom(targetId);
 
 //    this.contentDiv; //TODO create custom dom structure inside
 
-    var sampleHeight = this.height/this.samples.length;
-    for (var i = 0; i < this.samples.length; i++) {
-        var sample = this.samples[i];
-//        this.sampleDivs[sample] = $('<div id="' + sample + '-svgdiv" style="height:' + sampleHeight + 'px"></div>')[0];
-        _this.sampleDivs[sample] = $('<div id="' + sample + '-svgdiv" style="overflow-y: scroll"></div>')[0];
+    var fileHeight = this.height/this.files.length;
+    for (var i = 0; i < this.files.length; i++) {
+        var file = this.files[i];
+//        this.fileDivs[file] = $('<div id="' + file + '-svgdiv" style="height:' + fileHeight + 'px"></div>')[0];
+        _this.fileDivs[file] = $('<div id="' + file + '-svgdiv" style="overflow-y: scroll"></div>')[0];
 
         // TODO test $(this.contentDiv).css({'height': this.height});
-        $(_this.contentDiv).append(_this.sampleDivs[sample]);
+        $(_this.contentDiv).append(_this.fileDivs[file]);
 
-        _this.sampleMainSvgs[sample] = SVG.addChild(_this.sampleDivs[sample], 'svg', {
-            'class': 'sampleMainSvg',
+        _this.fileMainSvgs[file] = SVG.addChild(_this.fileDivs[file], 'svg', {
+            'class': 'fileMainSvg',
             'x': 0,
             'y': 0,
             'width': this.width,
-            'height': sampleHeight
+            'height': fileHeight
         });
         /* Internal svg structure */
-        _this.svgGroups[sample] = SVG.addChild(_this.sampleMainSvgs[sample], 'svg', {
+        _this.svgGroups[file] = SVG.addChild(_this.fileMainSvgs[file], 'svg', {
             'class': 'svgGroup',
             'x': -_this.pixelPosition,
             'width': _this.svgCanvasWidth
-//            'height': sampleHeight
+//            'height': fileHeight
         });
-        _this.renderers[sample].init(_this.svgGroups[sample], sample);
+        _this.renderers[file].init(_this.svgGroups[file], file);
     }
 
     this.svgCanvasOffset = (this.width * 3 / 2) / this.pixelBase;
@@ -188,7 +188,7 @@ MultisampleTrack.prototype.render = function (targetId) {
     this.updateHeight();
 };
 
-MultisampleTrack.prototype.draw = function () {
+MultifileTrack.prototype.draw = function () {
     var _this = this;
 
     this.svgCanvasOffset = (this.width * 3 / 2) / this.pixelBase;
@@ -207,7 +207,7 @@ MultisampleTrack.prototype.draw = function () {
     if (typeof this.visibleRegionSize === 'undefined' || this.region.length() < this.visibleRegionSize) {
         this.setLoading(true);
         this.dataAdapter.getData({
-            categories: this.samples,
+            categories: this.files,
             dataType: this.dataType,
             region: new Region({
                 chromosome: this.region.chromosome,
@@ -235,7 +235,7 @@ MultisampleTrack.prototype.draw = function () {
 };
 
 
-MultisampleTrack.prototype.move = function (disp) {
+MultifileTrack.prototype.move = function (disp) {
     var _this = this;
 
     this.dataType = 'features';
@@ -247,10 +247,10 @@ MultisampleTrack.prototype.move = function (disp) {
     var pixelDisplacement = disp * _this.pixelBase;
     this.pixelPosition -= pixelDisplacement;
 
-    for (var sample in this.svgGroups) {
+    for (var file in this.svgGroups) {
         //parseFloat important
-        var move = parseFloat(this.svgGroups[sample].getAttribute("x")) + pixelDisplacement;
-        this.svgGroups[sample].setAttribute("x", move);
+        var move = parseFloat(this.svgGroups[file].getAttribute("x")) + pixelDisplacement;
+        this.svgGroups[file].setAttribute("x", move);
     }
 
     var virtualStart = parseInt(this.region.start - this.svgCanvasOffset);
@@ -260,7 +260,7 @@ MultisampleTrack.prototype.move = function (disp) {
 
         if (disp > 0 && virtualStart < this.svgCanvasLeftLimit) {
             this.dataAdapter.getData({
-                categories: this.samples,
+                categories: this.files,
                 dataType: this.dataType,
                 region: new Region({
                     chromosome: _this.region.chromosome,
@@ -284,7 +284,7 @@ MultisampleTrack.prototype.move = function (disp) {
 
         if (disp < 0 && virtualEnd > this.svgCanvasRightLimit) {
             this.dataAdapter.getData({
-                categories: this.samples,
+                categories: this.files,
                 dataType: this.dataType,
                 region: new Region({
                     chromosome: _this.region.chromosome,
@@ -310,7 +310,7 @@ MultisampleTrack.prototype.move = function (disp) {
 
 };
 
-MultisampleTrack.prototype.dataReady = function (response) {
+MultifileTrack.prototype.dataReady = function (response) {
     var _this = this;
     var features;
     if (response.dataType == 'histogram') {
@@ -338,15 +338,15 @@ MultisampleTrack.prototype.dataReady = function (response) {
         resource: _this.resource,
         species: _this.species,
         featureType: _this.featureType, // FIXME
-        sample: response.category
-//        height: _this.sampleDivs[response.category].offsetHeight
+        file: response.category
+//        height: _this.fileDivs[response.category].offsetHeight
         //, params: response.params
     });
     _this.updateHeight();
 };
 
 /*
-MultisampleTrack.prototype.getFeaturesToRenderByChunk = function (response, filters) {
+MultifileTrack.prototype.getFeaturesToRenderByChunk = function (response, filters) {
     //Returns an array avoiding already drawn features in this.chunksDisplayed
 
     var getChunkId = function (position) {
@@ -396,7 +396,7 @@ MultisampleTrack.prototype.getFeaturesToRenderByChunk = function (response, filt
 };
 */
     /* when memoryStore is refactored to manage several categories*/
-MultisampleTrack.prototype.getFeaturesToRenderByChunk = function(response) {  // TODO test
+MultifileTrack.prototype.getFeaturesToRenderByChunk = function(response) {  // TODO test
     var _this = this;
 
     var chunks = response.items;
